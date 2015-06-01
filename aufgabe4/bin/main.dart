9d6dfcfc21d21315ec1f67830c6f1ec071b6b29d
Jhary <jhary82@gmx.de>
2015-06-01 14:38:33 +0200

    heruausfiltern von script

--- aufgabe4/bin/main.dart
+++ aufgabe4/bin/main.dart
@@ -14,8 +14,10 @@ main() async{
     print("Bitte geben Sie die URL ein, die sie auf ihre Zeichenhäufigkeiten untersuchen möchten:");  
     
     do {
-      url = "http://elpais.com/";
-      //url = stdin.readLineSync();
+      //url = "http://elpais.com/";
+      url = stdin.readLineSync();
+      
+     
       //Überprüfen der URL
       await http.get(url).then( (e) {      
         //Abfrage, ob url-type html ist
@@ -30,34 +32,29 @@ main() async{
       }).catchError( (e) {
         //falsche url    
         valid = false;        
-        print("sinnlose Eingabe\nURL '$url' konnte nicht verarbeitet werden.");
+        print("URL '$url' konnte nicht verarbeitet werden.");
       });    
     }while( !valid );
-     
+        
      //Auslesen des Dom-trees    
       var document = parse(urlHtml);         
       
       String str = "";
       int maxLength = 0;//Anzahl aller Zeichen
+            
+      str += document.body.text;
       
-      //durchsuche den Baum rekursiv nach text-Elementen
-      var list = document.children;
-      
-      
-      while( list.isNotEmpty ){
-        Element elem = list.last;
-        list.removeLast();
-        if(elem.hasContent()){
-          str += elem.text;
-          list.addAll(elem.children);
-        }
-      }
-      
-      //str += document.body.text;
-      
+      //Filtere Script Elemente aus
+      List<Element> skripte = document.body.querySelectorAll("script");      
+      skripte.forEach( (f) => str = str.replaceAll(f.text, ""));
+      //Filtere Style Elemente aus
+      List<Element> style = document.body.querySelectorAll("style");      
+      style.forEach( (f) => str = str.replaceAll(f.text, ""));
+                 
+      print(str);
       str = str.toLowerCase();// alles kleinschreiben      
       Map<String, int> map = new Map<String, int>();
-      
+            
       //filtere alles ausser a-z raus 
       str.splitMapJoin( (new RegExp("[a-z]")),
                 onMatch:    (m) {
@@ -65,8 +62,8 @@ main() async{
                   maxLength++;
                   map[str] = 1 + (map.containsKey(str)? map[str] : 0);
                 },
-                onNonMatch: (n) => '');   
-
+                onNonMatch: (n) => '');    
+      
       //sortiere die map und gebe aus       
       var sortList = map.keys.toList()..sort((a,b)=> map[b] - map[a]);
       sortList.forEach( (e) {        
